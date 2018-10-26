07f999ae2735526344b795c5f0a224f3cb606a44
JJ <jack.brown.255@gmail.com>
2018-10-25 22:13:44 -0500

    Added automatic 'turbo' mode to ML agent, so that we're just waiting on the AI (not the animation)

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ require "actions"
 require "gamestate"
 require "strategies"
 
+emu.speedmode("nothrottle") -- turn off pause between frames
+
 enterGame()
 --playSarsaGame(1e-4, 0.9)
 playQLearning(1e-4, 0.9, 'scripted')
